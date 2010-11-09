88b4ecdcad5e3d4f3613f2a9c13998de4705a43c
bensternthal <ben@devpatch.com>
2010-11-09 15:31:39 -0800

    upgraded jquery 1.4.3 fixed unit tests ie/ff

--- jquery.ui.dpui.wideScroller/tests/wideScroller_methods.js
+++ jquery.ui.dpui.wideScroller/tests/wideScroller_methods.js
@@ -8,8 +8,6 @@
 module("wideScroller: ui core");
 test("Number of items display", function() {
 
-    var el = $("#scroller").wideScroller();
-
     expect(2);
 
 	var value = $('.ws-totalItems').html();
@@ -23,27 +21,33 @@ test("Number of items display", function() {
 test("1st Item Location/Offset", function() {
     expect(1);
 
+    stop();
+    var locatorOffset = $("#locator").offset().left;
 
-    var locatorOffset = $("#locator").offset().left + "px";
-    var itemOffset = $(".active").css("left");
 
-    equals( locatorOffset, itemOffset, "Offest of locator and active image match" );
+    var timer = setInterval(function(){test()}, 1000);
 
+    function test() {
+        var itemOffset = $(".active").offset().left;
+        equals( locatorOffset, itemOffset, "Offest of locator and active image match, might fail if scrollbar FF, but OK" );
+        clearInterval(timer);
+        start();
+    }
 
 });
 
 module("wideScroller: events & callbacks");
-test("Stop & Start Callback", function() {
+test("Next Callback & Stop/Start Callback", function() {
     stop();
-    expect(2);
+    expect(3);
 
-    var startCount = 0;
-    var stopCount = 0;
-
+    var startCount = 0,
+        stopCount = 0,
+        next = 0;
 
     $("#scroller").wideScroller("option", "startScroll", function(){ startCount++;});
     $("#scroller").wideScroller("option", "stopScroll", function(){ stopCount++; });
-
+	$("#scroller").wideScroller("option", "next", function(){ next++; });
 
     $('.next').click();
 
@@ -52,58 +56,34 @@ test("Stop & Start Callback", function() {
     function test() {
         equals( startCount, 1, "start callback happened once" );
         equals( stopCount, 1, "stop callback happened once" );
+        equals( next, 1, "next button bound -> next callback happened once" );
         clearInterval(timer);
         start();
     }
+
 });
 
-test("Bind Controls, Prev/Next Callback", function() {
+test("Prev Callback & Stop/Start Callback", function() {
     stop();
-    expect(2);
+    expect(3);
 
-    var next = 0, prev = 0;
+    var startCount = 0,
+        stopCount = 0,
+        prev = 0;
 
-	$("#scroller").wideScroller("option", "next", function(){ next++; });
+    $("#scroller").wideScroller("option", "startScroll", function(){ startCount++;});
+    $("#scroller").wideScroller("option", "stopScroll", function(){ stopCount++; });
     $("#scroller").wideScroller("option", "previous", function(){ prev++; });
 
-    $('.next').click();
-	equals( next, 1, "next button bound -> next callback happened once" );
+
+    $('.prev').click();
 
     var timer = setInterval(function(){test()}, 4000);
 
     function test() {
-        $('.prev').click();
-	    equals( prev, 1, "prev button bound -> prev callback happened once" );
-        clearInterval(timer);
-        start();
-    }
-});
-
-
-
-test("Window Resize Event & Callback", function() {
-    expect(2);
-
-    stop()
-
-    var origWidth = $(window).width();
-    var origHeight = $(window).height();
-
-    var resize = 0;
-    $("#scroller").wideScroller("option", "resize", function(){ resize++; });
-
-
-    window.resizeTo(800,600);
-    var timer = setInterval(function(){test()}, 1000);
-
-    function test() {
-        var locatorOffset = $("#locator").offset().left + "px";
-        var itemOffset = $(".active").css("left");
-        equals( locatorOffset, itemOffset, "Offest of locator and active image match" );
-        ok( resize !== 0, "Resize callback happened at least once" );
-
-
-        window.resizeTo(origWidth, origHeight);
+        equals( startCount, 1, "start callback happened once" );
+        equals( stopCount, 1, "stop callback happened once" );
+        equals( prev, 1, "prev button bound -> prev callback happened once" );
         clearInterval(timer);
         start();
     }
@@ -123,12 +103,47 @@ test("Specify Start Image", function() {
         goToItem: '3'
     });
 
-    ok($(items[2]).hasClass("active"), "Second Image Is Active" );
+    ok($(items[2]).hasClass("active"), "Third Image Is Active" );
+
 
 });
 
 
-//TODO Test re-order
+
+
+/*
+
+Does not work well enough cross browser to use, problems with IE, Chrome
+test("Window Resize Event & Callback", function() {
+    expect(2);
+
+    stop();
+
+    var origWidth = $(window).width();
+    var origHeight = $(window).height();
+
+    var resize = 0;
+    $("#scroller").wideScroller("option", "resize", function(){ resize++; });
+
+
+    window.resizeTo(2000,1000);
+    var timer = setInterval(function(){test()}, 1000);
+
+    function test() {
+        var locatorOffset = $("#locator").offset().left + "px";
+        var itemOffset = $(".active").css("left");
+        equals( locatorOffset, itemOffset, "Offest of locator and active image match" );
+        ok( resize !== 0, "Resize callback happened at least once (wont work in chrome/ie since resizeTo used in test)" );
+
+        console.log(locatorOffset);
+        //window.resizeTo(origWidth, origHeight);
+        clearInterval(timer);
+        start();
+    }
+
+});
+*/
+
 //TODO Test Spinner
 
 
